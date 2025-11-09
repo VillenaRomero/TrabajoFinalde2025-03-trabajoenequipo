4288782f2f03c42f1b8c10ea67e5c2e0254da6af
alexander.villena@tecsup.edu.pe <alexander.villena@tecsup.edu.pe>
2025-11-08 21:01:44 -0500

    avance, aliminacion, y agreacion de modelo 3D

--- Folder_ProyectoUnity/PF_JuegoDeTerror3d/Assets/Scripts/ShowText.cs
+++ Folder_ProyectoUnity/PF_JuegoDeTerror3d/Assets/Scripts/ShowText.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RussianRoulette : MonoBehaviour
+public class ShowText : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
