c8e966ce290ec85a468299a2151ea41d8fb643ec
JoseLeq77 <jose.lequernaque@tecsup.edu.pe>
2025-11-16 23:47:32 -0500

    Menu Light Dotween movement added

--- Folder_ProyectoUnity/PF_JuegoDeTerror3d/Assets/Scripts/Game/LightMenu.cs
+++ Folder_ProyectoUnity/PF_JuegoDeTerror3d/Assets/Scripts/Game/LightMenu.cs
@@ -1,34 +1,48 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using DG.Tweening;
 
 public class LightMenu : MonoBehaviour
 {
-
+    [Header("Light Flicker Settings")]
     public Light light3D;
     public float minIntensity;
     public float maxIntensity;
     public float flickerSpeed;
     public float offChance;
 
+    [Header("Pendulum Settings")]
+    [SerializeField] private float pendulumAngle = 30f;
+    [SerializeField] private float pendulumDuration = 2f;
+    [SerializeField] private AnimationCurve pendulumCurve;
+    [SerializeField] private PendulumAxis rotationAxis = PendulumAxis.Z;
+
     private float timer;
+    private Tween _pendulumTween;
+    private Vector3 _initialRotation;
 
     public TMP_Text[] textostmpo;
     public Color color;
+
+    private enum PendulumAxis { X, Y, Z }
 
     void Start()
     {
         if (light3D == null)
             light3D = GetComponent<Light>();
         color.a = 10f;
+
+        _initialRotation = transform.localEulerAngles;
+        StartPendulumMotion();
     }
 
     void Update()
     {
         Efectlight();
-        
     }
-    void Efectlight(){
+
+    void Efectlight()
+    {
         timer += Time.deltaTime;
 
         if (timer >= flickerSpeed)
@@ -45,5 +59,32 @@ public class LightMenu : MonoBehaviour
 
             timer = 0f;
         }
+    }
+
+    private void StartPendulumMotion()
+    {
+        _pendulumTween?.Kill();
+
+        Vector3 targetRotation = _initialRotation;
+
+        switch (rotationAxis)
+        {
+            case PendulumAxis.X:
+                targetRotation = new Vector3(_initialRotation.x + pendulumAngle, _initialRotation.y, _initialRotation.z);
+                break;
+            case PendulumAxis.Y:
+                targetRotation = new Vector3(_initialRotation.x, _initialRotation.y + pendulumAngle, _initialRotation.z);
+                break;
+            case PendulumAxis.Z:
+                targetRotation = new Vector3(_initialRotation.x, _initialRotation.y, _initialRotation.z + pendulumAngle);
+                break;
+        }
+
+        _pendulumTween = transform.DOLocalRotate(targetRotation, pendulumDuration).SetEase(pendulumCurve).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void OnDisable()
+    {
+        _pendulumTween?.Kill();
     }
 }
